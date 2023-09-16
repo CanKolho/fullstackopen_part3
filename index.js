016a0fdd8e5b8aa34c165d03029d9b62f3030554
Can Kolho <can.kolho@hotmail.com>
2023-09-16 15:36:29 +0300

    3.10 puhelinluettelon backend step10

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -7,6 +8,7 @@ morgan.token('postData', function (req) {
   return req.method === 'POST' ? JSON.stringify(req.body) : ' '
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :postData')
@@ -102,7 +104,7 @@ app.post('/api/persons', (req, res) => {
   res.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
